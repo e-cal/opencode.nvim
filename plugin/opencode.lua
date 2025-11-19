35521c29b9ace3a550380103c77a7114a8167a2a
ecal <ethan@ecal.dev>
2025-11-19 13:48:29 -0500

    Add command

--- plugin/opencode.lua
+++ plugin/opencode.lua
@@ -1,37 +1,34 @@
 vim.api.nvim_create_autocmd("User", {
-  group = vim.api.nvim_create_augroup("OpencodeAutoReload", { clear = true }),
-  pattern = "OpencodeEvent",
-  callback = function(args)
-    local event = args.data.event
+	group = vim.api.nvim_create_augroup("OpencodeAutoReload", { clear = true }),
+	pattern = "OpencodeEvent",
+	callback = function(args)
+		local event = args.data.event
 
-    print(event.type)
+		print(event.type)
 
-    if event.type == "file.edited" and require("opencode.config").opts.auto_reload then
-      if not vim.o.autoread then
-        -- Unfortunately `autoread` is kinda necessary, for `:checktime`.
-        -- Alternatively we could `:edit!` but that would lose any unsaved changes.
-        vim.notify(
-          "Please set `vim.o.autoread = true` to use `opencode.nvim` auto-reload, or set `vim.g.opencode_opts.auto_reload = false`",
-          vim.log.levels.WARN,
-          { title = "opencode" }
-        )
-      else
-        -- `schedule` because blocking the event loop during rapid SSE influx can drop events
-        vim.schedule(function()
-          -- `:checktime` checks all buffers - no need to check the event's file
-          vim.cmd("checktime")
-        end)
-      end
-    end
-  end,
-  desc = "Reload buffers edited by `opencode`",
+		if event.type == "file.edited" then
+			if not vim.o.autoread then
+				-- Unfortunately `autoread` is kinda necessary, for `:checktime`.
+				-- Alternatively we could `:edit!` but that would lose any unsaved changes.
+				vim.notify(
+					"Please set `vim.o.autoread = true` to use `opencode.nvim` auto-reload, or set `vim.g.opencode_opts.auto_reload = false`",
+					vim.log.levels.WARN,
+					{ title = "opencode" }
+				)
+			else
+				-- `schedule` because blocking the event loop during rapid SSE influx can drop events
+				vim.schedule(function()
+					-- `:checktime` checks all buffers - no need to check the event's file
+					vim.cmd("checktime")
+				end)
+			end
+		end
+	end,
+	desc = "Reload buffers edited by `opencode`",
 })
 
-
-
---    vim.api.nvim_create_autocmd("User", {
---      pattern = "OpencodeEvent",
---      callback = function(event)
---        print("Received OpenCode event:", vim.inspect(event.data))
---      end,
---    })
+vim.api.nvim_create_user_command("OpencodeConnect", function()
+	require("opencode").subscribe_to_sse()
+end, {
+	desc = "Connect to OpenCode SSE server for auto-reloading files",
+})
